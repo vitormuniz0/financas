5c973c4b2aaf424b1bd35866ef0952893d833937
vitormuniz0 <vitormuniz311@gmail.com>
2024-09-06 22:22:07 -0300

    Corrigindo erros

--- server/src/connection/index.ts
+++ server/src/connection/index.ts
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize"
 
 export const sequelize = new Sequelize('financas', 'root', '', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'postgres'
 })
 
 const testConnection = async () => {
